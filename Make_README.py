e364b1dc6f6d3524b26f8a602bc95d77960372d4
Dezeli <haterecursive@gmail.com>
2023-08-25 22:48:26 +0900

    Update README

---
+++ Make_README.py
@@ -0,0 +1,22 @@
+import os
+
+Programing_Languages = ["Python"]
+readme = open(f"./README.md", "w", encoding="UTF-8")
+readme.write(
+    "## BaekJoon Online Judge Solutions   \nhttps://www.acmicpc.net/   \n"
+)
+for language in Programing_Languages:
+    readme.write(f"\n#### {language}\n")
+    files = os.listdir(f"./{language}")
+    file_list = []
+
+    for filename in files:
+        file = open(f"./{language}/{filename}", "r", encoding="UTF-8")
+        title = file.readline()[2:-1]
+        filenum = filename.split(".")[0]
+        file_list.append([int(filenum), title, file])
+    file_list.sort()
+
+    for filenum, title, file in file_list:
+        readme.write(f"- [{filenum}번 : {title}](./{language}/{file})    \n")
+readme.close()
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# BaekJoon Online Judge Solutions
+## BaekJoon Online Judge Solutions   
+https://www.acmicpc.net/   
+
+#### Python
+- [1000번 : A+B](./Python/<_io.TextIOWrapper name='./Python/1000.py' mode='r' encoding='UTF-8'>)    
+- [1001번 : A-B](./Python/<_io.TextIOWrapper name='./Python/1001.py' mode='r' encoding='UTF-8'>)    
+- [1008번 : A/B](./Python/<_io.TextIOWrapper name='./Python/1008.py' mode='r' encoding='UTF-8'>)    
