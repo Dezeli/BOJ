4d5ee0907f1840c2f180b7a8ab3b8c4974e8d6e2
Dezeli <haterecursive@gmail.com>
2023-09-03 00:52:36 +0900

    Solve

---
+++ Python/1284.py
@@ -0,0 +1,17 @@
+# House Numbers
+import sys
+
+while True:
+    num = sys.stdin.readline().rstrip()
+    if num == "0":
+        break
+    size = 1
+    for i in num:
+        if i == "0":
+            size += 5
+        elif i == "1":
+            size += 3
+        else:
+            size += 4
+    print(size)
+    
---
+++ Python/1296.py
@@ -0,0 +1,24 @@
+# 팀 이름 정하기
+import sys
+
+boyName = sys.stdin.readline().rstrip()
+N = int(sys.stdin.readline().rstrip())
+score = 0
+name = []
+bl = boyName.count("L")
+bo = boyName.count("O")
+bv = boyName.count("V")
+be = boyName.count("E")
+for _ in range(N):
+    girlNname = sys.stdin.readline().rstrip()
+    l = girlNname.count("L") + bl
+    o = girlNname.count("O") + bo
+    v = girlNname.count("V") + bv
+    e = girlNname.count("E") + be
+    newScore = ((l + o) * (l + v) * (l + e) * (o + v) * (o + e) * (v + e)) % 100
+    if score < newScore:
+        score = newScore
+        name = [girlNname]
+    elif score == newScore:
+        name.append(girlNname)
+print(sorted(name)[0])
