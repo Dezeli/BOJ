ff06085e0278d7963fc5a4804b31e18754dad2bd
Dezeli <haterecursive@gmail.com>
2023-09-07 10:09:39 +0900

    Solve

---
+++ Python/1546.py
@@ -0,0 +1,13 @@
+# 평균
+
+N = int(input())
+scoreList = input().split(" ")
+for i in range(len(scoreList)):
+    scoreList[i] = int(scoreList[i])
+scoreList.sort()
+for i in range(len(scoreList)):
+    scoreList[i] = int(scoreList[i]) / int(scoreList[-1]) * 100
+scoreSum = 0
+for i in scoreList:
+    scoreSum += float(i)
+print(scoreSum / N)
