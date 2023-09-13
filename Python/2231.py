a5586dd1950b6db7661d9bee79ea1ed00e87151d
Dezeli <haterecursive@gmail.com>
2023-09-13 11:57:57 +0900

    Solve

---
+++ Python/2231.py
@@ -0,0 +1,18 @@
+# Digit Generator
+
+N = int(input())
+checkStart = N - (len(str(N)) * 9)
+if checkStart <= 0:
+    checkStart = 1
+
+bunhaehap = 0
+for i in range(checkStart, N):
+    bunhaehap = 0
+    for j in str(i):
+        bunhaehap += int(j)
+    bunhaehap += i
+    if bunhaehap == N:
+        print(i)
+        break
+if bunhaehap != N:
+    print(0)
