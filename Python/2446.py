018a0bb0d61edb25705e7ed3f2f61cfb7d8023d7
Dezeli <haterecursive@gmail.com>
2023-09-16 10:56:53 +0900

    Solve

---
+++ Python/2446.py
@@ -0,0 +1,14 @@
+# 별 찍기 - 9
+
+N = int(input())
+
+if N != 1:
+    for i in range(N):
+        print(" " * i, end="")
+        print("*" * ((N - i) * 2 - 1), end="")
+        print("", end=" \n")
+    for i in range(1, N - 1):
+        print(" " * (N - i - 1), end="")
+        print("*" * ((i + 1) * 2 - 1), end="")
+        print("", end=" \n")
+print("*" * ((N) * 2 - 1), end="")
---
+++ Python/2475.py
@@ -0,0 +1,8 @@
+# 검증수
+import sys
+
+nums = list(map(int, sys.stdin.readline().split(" ")))
+sumList = []
+for i in nums:
+    sumList.append(i**2)
+print(sum(sumList) % 10)
