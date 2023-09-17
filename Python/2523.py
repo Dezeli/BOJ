0877badc58016bb7ef2b8be7653fb1da8799e43b
Dezeli <haterecursive@gmail.com>
2023-09-17 12:15:33 +0900

    Solve

---
+++ Python/2523.py
@@ -0,0 +1,11 @@
+# 별 찍기 - 13
+
+N = int(input())
+
+if N != 1:
+    for i in range(N):
+        print("*" * (i + 1), end=" \n")
+
+    for i in range(1, N - 1):
+        print("*" * (N - i), end="\n")
+print("*", end="")
