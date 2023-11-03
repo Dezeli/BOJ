053a7aa33b622839f6f7d75e8dbc93fdf99624dc
Dezeli <haterecursive@gmail.com>
2023-11-03 12:53:42 +0900

    Solve

---
+++ Python/1874.py
@@ -0,0 +1,31 @@
+# 스택 수열
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+nums = [i for i in range(n, 0, -1)]
+orders = [0]
+stack = [0]
+for _ in range(n):
+    sn = int(sys.stdin.readline().rstrip())
+    while True:
+        if orders[0]=="NO":
+            break
+        if sn == stack[-1]:
+            stack.pop()
+            orders.append("-")
+
+            break
+        else:
+            if nums:
+                stack.append(nums.pop())
+                orders.append("+")
+            else:
+                orders[0] = "NO"
+                break
+
+if orders[0]=="NO":
+    print("NO")
+else:
+    for order in orders[1:]:
+        print(order)
+        
