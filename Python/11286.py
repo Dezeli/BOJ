505c97132f9fc22973e0b7fac4f6d29fb86e2eda
Dezeli <haterecursive@gmail.com>
2023-11-12 00:30:17 +0900

    Solve

---
+++ Python/11286.py
@@ -0,0 +1,20 @@
+import sys
+import heapq
+
+N = int(sys.stdin.readline().rstrip())
+heap = []
+
+for _ in range(N):
+    x = int(sys.stdin.readline().rstrip())
+
+    if x==0:
+        if heap:
+            num, sign = heapq.heappop(heap)
+            print(num*sign)
+        else:
+            print(0)
+    else:
+        if x<0:
+            heapq.heappush(heap, [-x, -1])
+        else:
+            heapq.heappush(heap, [x, 1])
--- Python/1697.py
+++ Python/1697.py
@@ -1,34 +1,31 @@
 # 숨바꼭질
 import sys
-from collections import deque
-
 
 N, K = map(int, sys.stdin.readline().split(" "))
 
-ran = 100
+ran = 200000
 
 times = [0 for _ in range(ran+1)]
 
-for i in range(ran+1):
-    if i < N:
-        times[i] = N-i
-    elif i==N:
-        times[i] = 0
-    else:
-        if i%2==0:
-            times[i] = min(times[i-1]+1, times[i//2]+1)
-        else:
-            times[i] = times[i-1]+1
+cur = [N]
+next = []
+time = 0
 
-for i in range(ran-1, -1, -1):
-    times[i] = min([times[i], times[i+1]+1])
+if N==K:
+    print(0)
 
-for i in range(ran+1):
-    if i%2==0:
-        times[i] = min(times[i], times[i//2]+1)
-    else:
-        times[i] 
-
-print(times[K])
-for i in range(len(times)):
-    print(f"{i}. {times[i]}")
+else:
+    while times[K]==False:
+        while cur:
+            n = cur.pop()
+            if n<0 or n>ran:
+                continue
+            if times[n]:
+                continue
+            else:
+                times[n] = time
+                next += [n-1, n+1, n*2]
+        cur = next
+        next = []
+        time += 1
+    print(times[K])
