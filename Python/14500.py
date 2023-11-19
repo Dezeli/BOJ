99f2702bd67bb070a897646d34a5b7db8253d5f3
Dezeli <haterecursive@gmail.com>
2023-11-19 14:30:01 +0900

    Solve

--- Python/14500.py
+++ Python/14500.py
@@ -11,21 +11,48 @@ for _ in range(N):
     Paper.append(line)
 
 
-
-dir = [('L', -1, 0), ('R', 1, 0), ('U', 0, -1), ('D', 0, 1)]
+directions = [('L', -1, 0), ('R', 1, 0), ('U', 0, -1), ('D', 0, 1)]
 reverse_Dirs = {'L':'R', 'R':'L', 'U':'D', 'D':'U'}
-def add(before, sum_num, cnt, x, y):
+
+
+sum_list = []
+
+def add(shape, sum_num, cnt, x, y):
+    if x>=M or x<0:
+        return
+    if y>=N or y<0:
+        return
+    
     sum_num += Paper[y][x]
     cnt += 1
     if cnt == 4:
-        print(before, sum_num)
+        sum_list.append(sum_num)
         return
     
-    rev_dir = reverse_Dirs[before[-1]]
+    rev_dir = reverse_Dirs[shape[-1]]
+    if cnt == 2:
+        if shape=='R':
+            if x+1<M:
+                add(shape, sum_num + Paper[y][x+1], 3, x, y-1)
+                add(shape, sum_num + Paper[y][x+1], 3, x, y+1)
+        elif shape=='D':
+            if y+1<N:
+                add(shape, sum_num + Paper[y+1][x], 3, x-1, y)
+                add(shape, sum_num + Paper[y+1][x], 3, x+1, y)
 
-    for s, xp, yp in dir:
-        if s == rev_dir:
+
+    for dir, xp, yp in directions:
+        if dir==rev_dir:
             continue
-        add(before+s, sum_num, cnt, x+xp, y+yp)
 
-add('R', 0, 0, 1, 1)
+        add(shape+dir, sum_num, cnt, x + xp, y + yp)
+
+
+for i in range(M):
+    for j in range(N):
+        add('R', Paper[j][i], 1, i+1, j)
+        add('D', Paper[j][i], 1, i, j+1)
+        add('L', Paper[j][i], 1, i-1, j)
+        add('U', Paper[j][i], 1, i, j-1)
+
+print(max(sum_list))
