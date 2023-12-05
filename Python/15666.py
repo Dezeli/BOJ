77f5e8151a01a60c1ff038dd33c1c09c2f390d28
Dezeli <haterecursive@gmail.com>
2023-12-05 19:02:29 +0900

    Solve

---
+++ Python/15666.py
@@ -0,0 +1,26 @@
+# N과 M (12)
+import sys
+
+N, M = map(int, sys.stdin.readline().split(" "))
+nums = list(map(int, sys.stdin.readline().split(" ")))
+nums.sort()
+temp = []
+
+
+def DFS(N, M):
+    if len(temp)==M:
+        print(*temp)
+        return
+    
+    same = 0
+    for i in range(N):
+        if same != nums[i]:
+            if temp:
+                if temp[-1]>nums[i]:
+                    continue
+            temp.append(nums[i])
+            same = nums[i]
+            DFS(N, M)
+            temp.pop()
+
+DFS(N, M)
