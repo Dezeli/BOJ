22f82720e0f05749a7b5a48d523b2791dff463ab
Dezeli <haterecursive@gmail.com>
2023-12-12 09:37:21 +0900

    Solve

---
+++ Python/11660.py
@@ -0,0 +1,22 @@
+# 구간 합 구하기 5
+import sys
+
+input = sys.stdin.readline
+N, M = map(int, input().split(" "))
+
+table = []
+
+for _ in range(N):
+    table.append(list(map(int, input().split(" "))))
+    
+dp = [[0 for __ in range(N+1)] for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + table[i-1][j-1]
+
+for k in range(M):
+    x1, y1, x2, y2 = map(int, input().split())
+    
+    result = dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1]
+    print(result)
--- Python/9456.py
+++ Python/9456.py
@@ -1,2 +1,18 @@
 # 스티커 
 import sys
+
+T = int(sys.stdin.readline().rstrip())
+
+for _ in range(T):
+    n = int(sys.stdin.readline().rstrip())
+    line1 = list(map(int, sys.stdin.readline().split(" ")))
+    line2 = list(map(int, sys.stdin.readline().split(" ")))
+
+    zero = 0
+    one = 0
+    two = 0
+
+    for i in range(n):
+        zero, one, two = max([one, two]), line1[i] + max([zero, two]), line2[i] + max([zero, one])
+
+    print(max([zero, one, two]))
