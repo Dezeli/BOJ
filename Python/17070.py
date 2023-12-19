0b8605867f1bb93e740dadbfcc63c5502a361d7e
Dezeli <haterecursive@gmail.com>
2023-12-19 20:11:07 +0900

    Solve

--- Python/17070.py
+++ Python/17070.py
@@ -1,65 +1,27 @@
 # 파이프 옮기기 1
 import sys
-import copy
 
 input = sys.stdin.readline
-
 N = int(input())
-pipe_map = []
+pipe_map = [list(map(int, input().split())) for _ in range(N)]
+dp = [[[0 for _ in range(N)] for _ in range(N)] for _ in range(3)]
+# dp[0] -> 가로, dp[1] -> 대각선, dp[2] -> 세로
 
-for _ in range(N):
-    line = list(map(int, sys.stdin.readline().split()))
-    pipe_map.append(line)
-
-print(map)
-
-def move(pipe, dir):
-    print(pipe)
-    if pipe[-1] == [N-1, N-1]:
-        print(111)
-        return 1
+def move_pipe():
+    dp[0][0][1] = 1
+    for i in range(2, N):
+        if pipe_map[0][i] == 0:
+            dp[0][0][i] = dp[0][0][i - 1]
+	
+    for r in range(1, N):
+        for c in range(1, N):
+            if pipe_map[r][c] == 0 and pipe_map[r][c - 1] == 0 and pipe_map[r - 1][c] == 0:
+                dp[1][r][c] = dp[0][r - 1][c - 1] + dp[1][r - 1][c - 1] + dp[2][r - 1][c - 1]
+                
+            if pipe_map[r][c] == 0:
+                dp[0][r][c] = dp[0][r][c - 1] + dp[1][r][c - 1]
+                dp[2][r][c] = dp[2][r - 1][c] + dp[1][r - 1][c]
     
-    if dir == "diagonal":
-        for i, j in pipe:
-            if i<0 or i>=N:
-                return 0
-            if j<0 or j>=N:
-                return 0
-            if map[i][j]:
-                return 0
-        if map[i-1][j]:
-            return 0
-        if map[i][j]=="1":
-            return 0
-    else:
-        for i, j in pipe:
-            if i<0 or i>=N:
-                return 0
-            if j<0 or j>=N:
-                return 0
-            if map[i][j]=="1":
-                print(map[i][j])
-                print("123123")
-                return 0
+    print(sum(dp[i][N - 1][N - 1] for i in range(3)))
 
-    print("111111111111")
-    last_pipe = pipe[-1]
-    horizontal_pipe = copy.deepcopy(last_pipe)
-    horizontal_pipe[1] += 1
-    verticle_pipe = copy.deepcopy(last_pipe)
-    verticle_pipe[0] += 1
-    diagonal_pipe = copy.deepcopy(last_pipe)
-    diagonal_pipe[0] += 1
-    diagonal_pipe[1] += 1
-
-    if dir=="horizontal":
-        return move([last_pipe, horizontal_pipe], "horizontal") + move([last_pipe, diagonal_pipe], "diagonal")
-    elif dir=="vertical":
-        return move([last_pipe, verticle_pipe], "verticle") + move([last_pipe, diagonal_pipe], "diagonal")
-    elif dir=="diagonal":
-        return move([last_pipe, horizontal_pipe], "horizontal") + move([last_pipe, verticle_pipe], "verticle") + move([last_pipe, diagonal_pipe], "diagonal")
-    
-
-
-print(move([[0, 0], [0, 1]], "horizontal"))
-
+move_pipe()
