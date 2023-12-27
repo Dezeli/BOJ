61b4d39a3157747972e988d69dfc6f5f1a9674b5
Dezeli <haterecursive@gmail.com>
2023-12-27 19:46:20 +0900

    Solve

--- Python/1504.py
+++ Python/1504.py
@@ -1,44 +1,35 @@
 # 특정한 최단 경로
 import sys
+import heapq
 from collections import defaultdict
 
 input = sys.stdin.readline
-
 N, E = map(int, input().split(" "))
-
 Graph = defaultdict(list)
 
 for _ in range(E):
     u, v, d = map(int, input().split(" "))
-    Graph[u].append([v, d])
-    Graph[v].append([u, d])
-
+    Graph[u].append([d, v])
+    Graph[v].append([d, u])
 v1, v2 = map(int, input().split(" "))
-print(Graph)
-
 
 def short_move(start, final):
     short_distance = [sys.maxsize for _ in range(N+1)]
     short_distance[start] = 0
-
-    moves = []
-    for v, d in Graph[start]:
-        moves.append([v, d])
-
+    moves = [[0, start]]
     while moves:
-        v, d = moves.pop()
-        if v==final:
-            short_distance[final] = min([short_distance[final], d])
-            continue
-        else:
-            if short_distance[final]<=d:
-                continue
-            else:
-                short_distance[final] = d
-                for newv, newd in Graph[start]:
-                    moves.append([newv, newd+d])
-    print(short_distance)
+        d, u = heapq.heappop(moves)
+        if d > short_distance[u]: continue
+        for w, v in Graph[u]:
+            if short_distance[v] > short_distance[u] + w:
+                short_distance[v] = short_distance[u] + w
+                heapq.heappush(moves, [short_distance[v], v])
     return short_distance[final]
 
+route1 = short_move(1, v1)+ short_move(v1, v2) + short_move(v2, N)
+route2 = short_move(1, v2)+ short_move(v2, v1) + short_move(v1, N)
 
-print(short_move(1, 4))
+if min([route1, route2]) < sys.maxsize:
+    print(min([route1, route2]))
+else:
+    print(-1)
