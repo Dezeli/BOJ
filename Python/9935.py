cf68175eade6748c4f48884af8b7f43a807ad50f
Dezeli <haterecursive@gmail.com>
2024-01-09 09:44:54 +0900

    Solve

---
+++ Python/9935.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+s = input().rstrip()
+b = input().rstrip()
+len_b = len(b)
+
+stack = []
+
+for i in s:
+    stack.append(i)
+    if len(stack)>=len_b:
+        if ''.join(stack[-len_b:])==b:
+            for i in range(len_b):
+                stack.pop()
+
+if stack:
+    print(''.join(stack))
+else:
+    print("FRULA")
