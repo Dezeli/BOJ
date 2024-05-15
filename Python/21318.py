eac13813fe5b23e7f0aaaa9689e7391c901182ac
Dezeli <haterecursive@gmail.com>
2024-05-15 16:55:40 +0900

    Solve

---
+++ Python/21318.py
@@ -0,0 +1,22 @@
+# 피아노 체조
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+level = list(map(int, input().split()))
+
+p_play = [0]
+
+for i in range(N-1):
+    if level[i] > level[i+1]:
+        p_play.append(p_play[-1]+1)
+    else:
+        p_play.append(p_play[-1])
+p_play.append(p_play[-1])
+
+
+Q = int(input())
+for _ in range(Q):
+    s, e = map(int, input().split())
+    print(p_play[e-1] - p_play[s-1])
