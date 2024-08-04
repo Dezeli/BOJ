b68e7489e043cdc83021fe542e167d25a642eaa7
Dezeli <haterecursive@gmail.com>
2024-08-04 21:33:31 +0900

    Solve

---
+++ Python/12931.py
@@ -0,0 +1,23 @@
+# 두 배 더하기
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+
+max_mul = 0
+sum_plus = 0
+
+for num in A:
+    mul = 0
+    while num:
+        if num%2==0:
+            num = num//2
+            mul += 1 
+        else:
+            num -= 1
+            sum_plus += 1
+    max_mul = max(max_mul, mul)
+
+print(max_mul + sum_plus)
