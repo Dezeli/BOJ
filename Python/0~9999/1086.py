cbd1cd6c59610337af28ce687c406e7e6a400985
Dezeli <haterecursive@gmail.com>
2024-12-14 14:23:38 +0900

    Solve

--- Python/0~9999/1086.py
+++ Python/0~9999/1086.py
@@ -1,19 +1,42 @@
 # 박성원
 import sys
-import itertools
+from math import gcd
 
 input = sys.stdin.readline
 
-N = int(input())
-nums = [int(input()) for _ in range(N)]
-K = int(input())
-nums = [str(i) for i in nums]
 
+N = int(input().strip())
+nums = [input().strip() for _ in range(N)]
+K = int(input().strip())
 
-case = list(itertools.permutations(nums, N))
-cnt = 0
+mod_nums = []
+for num in nums:
+    mod_nums.append(int(num) % K)
 
-for i in case:
-    if int(''.join(i))%K==0:
-        cnt += 1
-print(cnt)
+mod_squares = [1] * 51
+for i in range(1, 51):
+    mod_squares[i] = (mod_squares[i-1] * 10) % K
+
+dp = [[0] * K for _ in range(2**N)]
+dp[0][0] = 1
+
+for mask in range(2**N):
+    for rest in range(K):
+        if dp[mask][rest] == 0:
+            continue
+        for i in range(N):
+            if not (mask & (1 << i)):
+                new_mask = mask | (1 << i)
+                new_rest = (rest*mod_squares[len(nums[i])] + mod_nums[i]) % K
+                dp[new_mask][new_rest] += dp[mask][rest]
+
+pos = dp[(1 << N) - 1][0]
+case = 1
+for i in range(2, N + 1):
+    case *= i
+
+if pos == 0:
+    print("0/1")
+else:
+    div = gcd(pos, case)
+    print(f"{pos//div}/{case//div}")
---
+++ Python/0~9999/3015.py
@@ -0,0 +1,23 @@
+# 오아시스 재결합
+import sys
+from bisect import bisect_left, bisect_right
+
+input = sys.stdin.readline
+
+N = int(input())
+
+stack = []
+cnt = 0
+
+for _ in range(N):
+    h = -int(input())
+    i = bisect_left(stack, h)
+    if i==0:
+        cnt += len(stack) - i
+    else:
+        cnt += len(stack) - i + 1 
+    while len(stack) > bisect_right(stack, h):
+        stack.pop()
+    stack.append(h)
+
+print(cnt)
