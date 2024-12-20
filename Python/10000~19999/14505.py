779d455674ffdc358e6b53f20ee8f844a416e3c2
Dezeli <haterecursive@gmail.com>
2024-12-20 19:53:20 +0900

    Solve

---
+++ Python/10000~19999/14505.py
@@ -0,0 +1,18 @@
+# 팰린드롬 개수 구하기 (Small)
+import sys
+
+input = sys.stdin.readline
+
+
+S = ' ' + input().rstrip()
+N = len(S) - 1
+
+dp = [0]*(N+1)
+prefix_sum_dp = [0]*(N+1)
+for l in range(1, N+1):
+    for r in range(l, N+1):
+        if S[l] == S[r]:
+            dp[r] += prefix_sum_dp[N] - prefix_sum_dp[r] + 1
+        prefix_sum_dp[r] = (prefix_sum_dp[r - 1] + dp[r])
+
+print(prefix_sum_dp[N])
