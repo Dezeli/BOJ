3778be9bd1f618aba5f818a26bc93c4561b36031
Dezeli <haterecursive@gmail.com>
2024-12-28 22:07:34 +0900

    Solve

---
+++ Python/10000~19999/17224.py
@@ -0,0 +1,22 @@
+# APC는 왜 서브태스크 대회가 되었을까?
+import sys
+
+input = sys.stdin.readline
+
+N, L, K = map(int, input().split())
+
+s1_cnt = 0
+s2_cnt = 0
+
+for _ in range(N):
+    s1, s2 = map(int, input().split())
+    if s2<=L:
+        s2_cnt += 1
+    else:
+        if s1<=L:
+            s1_cnt += 1
+
+if s2_cnt>=K:
+    print(140*K)
+else:
+    print(140*s2_cnt+100*(min(s1_cnt, K-s2_cnt)))
