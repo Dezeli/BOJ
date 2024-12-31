798ddb088a5dd9010bb915f0dd73ed634e38687c
Dezeli <haterecursive@gmail.com>
2024-12-31 14:20:30 +0900

    Solve

---
+++ Python/30000~39999/32941.py
@@ -0,0 +1,10 @@
+# 왜 맘대로 예약하냐고
+
+T, X = map(int, input().split())
+N = int(input())
+
+for _ in range(N):
+    K = int(input()) 
+    A = list(map(int, input().split()))
+
+    
---
+++ Python/30000~39999/32951.py
@@ -0,0 +1,5 @@
+# AI 선도대학
+
+N = int(input())
+
+print(N-2024)
