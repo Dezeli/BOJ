4561bb2c44e2c224beb879f7e84212b1d81c1823
Dezeli <haterecursive@gmail.com>
2025-01-03 21:33:43 +0900

    Solve

---
+++ Python/0~9999/5585.py
@@ -0,0 +1,15 @@
+# 거스름돈
+import sys
+
+input = sys.stdin.readline
+
+N = 1000 - int(input())
+
+cnt = 0
+money = [500, 100, 50, 10, 5, 1]
+for m in money:
+    c = N//m
+    N -= m*c
+    cnt += c
+
+print(cnt)
