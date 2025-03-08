f6c0e7a3cb212a7c6e5a51303b2ec078424989ef
Dezeli <haterecursive@gmail.com>
2025-03-08 17:18:23 +0900

    Solve

---
+++ Python/0~9999/1092.py
@@ -0,0 +1,31 @@
+# 배
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N = int(input())
+cranes = list(map(int, input().split()))
+cranes.sort(reverse=True)
+
+M = int(input())
+boxes = list(map(int, input().split()))
+boxes.sort(reverse=True)
+boxes = deque(boxes)
+
+if cranes[0]<boxes[0]:
+    print(-1)
+else:
+    cnt = 0
+    while boxes:
+        cnt += 1
+        next_boxes = deque([])
+        i = 0
+        while boxes and i<N:
+            b = boxes.popleft()
+            if cranes[i]>=b:
+                i += 1
+            else:
+                next_boxes.append(b)
+        boxes = next_boxes + boxes
+    print(cnt)
---
+++ Python/0~9999/3109.py
@@ -0,0 +1,42 @@
+# 빵집
+import sys
+
+input = sys.stdin.readline
+sys.setrecursionlimit(10**5)
+
+R, C = map(int, input().split())
+
+roads = [[i for i in input().rstrip()] for _ in range(R)]
+visit = [[0]*C for _ in range(R)]
+
+def DFS(y, x):
+    if y<0 or y>=R:
+        return False
+    
+    if visit[y][x]==1:
+        return False
+    visit[y][x] = 1
+
+    if x==C-1:
+        return True
+
+    if roads[y][x]=='x':
+        return False
+    
+    r1 = DFS(y-1, x+1)
+    if r1:
+        return True
+    r2 = DFS(y, x+1)
+    if r2:
+        return True
+    r3 = DFS(y+1, x+1)
+    if r3:
+        return True
+    return False
+
+cnt = 0
+for i in range(R):
+    result = DFS(i, 0)
+    if result:
+        cnt += 1
+print(cnt)
