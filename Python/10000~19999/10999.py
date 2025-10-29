1f1d315204d052a7497bebf31c71e1b7a5946310
Dezeli <haterecursive@gmail.com>
2025-10-29 14:31:56 +0900

    Solve

--- Python/10000~19999/10999.py
+++ Python/10000~19999/10999.py
@@ -2,78 +2,69 @@
 import sys
 
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
 N, M, K = map(int, input().split())
 data = list(map(int, sys.stdin.read().split()))
+
 nums = [0] + data[:N]
-segment_tree = [0]*(N*4)
-lazy_tree = [0]*(N*4)
-
-def make_tree(s, e, i):
-    if s==e:
-        segment_tree[i] = nums[s]
-        return segment_tree[i]
-    
-    mid = (s+e)//2
-    left = make_tree(s, mid, i*2)
-    right = make_tree(mid+1, e, i*2+1)
-    segment_tree[i] = left + right
-    return segment_tree[i]
-
-make_tree(1, N, 1)
-
-def lazy_update(s, e, b, c, d, i):
-    if e<b or s>c:
-        return
-    if b<=s and e<=c:
-        lazy_tree[i] += d
-        return
-
-    mid = (s+e)//2
-    lazy_update(s, mid, b, c, d, i*2)
-    lazy_update(mid+1, e, b, c, d, i*2+1)
-    return
-
-def change_tree(s, e, b, c, d, i):
-    if e<b or s>c:
-        lazy_tree[i] = d
-        return
-    if s==e:
-        segment_tree[i] += d
-        return segment_tree[i]
-
-    mid = (s+e)//2
-    left = change_tree(s, mid, b, c, d + lazy_tree[i], i*2)
-    right = change_tree(mid+1, e, b, c, d + lazy_tree[i], i*2+1)
-    segment_tree[i] = left + right
-    return segment_tree[i]
-
-def get_sum(s, e, b, c, i):
-    if e<b or s>c:
-        return 0
-    
-    if lazy_tree[i]:
-        change_tree(s, e, b, c, 0, i)
-        lazy_tree[i]= 0
-        
-    if s>=b and e<=c:
-        return segment_tree[i]
-    
-    mid = (s+e)//2
-    left = get_sum(s, mid, b, c, i*2)
-    right = get_sum(mid+1, e, b, c, i*2+1)
-    return left+right
-
 queries = data[N:]
+
+segment_tree = [0] * (N * 4)
+lazy_tree = [0] * (N * 4)
+
+def build(node, start, end):
+    if start == end:
+        segment_tree[node] = nums[start]
+        return segment_tree[node]
+    mid = (start + end) // 2
+    segment_tree[node] = build(node*2, start, mid) + build(node*2+1, mid+1, end)
+    return segment_tree[node]
+
+def propagate(node, start, end):
+    if lazy_tree[node] != 0:
+        segment_tree[node] += (end - start + 1) * lazy_tree[node]
+        if start != end:
+            lazy_tree[node*2] += lazy_tree[node]
+            lazy_tree[node*2+1] += lazy_tree[node]
+        lazy_tree[node] = 0
+
+def update(node, start, end, left, right, diff):
+    propagate(node, start, end)
+    if end < left or start > right:
+        return
+    if left <= start and end <= right:
+        segment_tree[node] += (end - start + 1) * diff
+        if start != end:
+            lazy_tree[node*2] += diff
+            lazy_tree[node*2+1] += diff
+        return
+    mid = (start + end) // 2
+    update(node*2, start, mid, left, right, diff)
+    update(node*2+1, mid+1, end, left, right, diff)
+    segment_tree[node] = segment_tree[node*2] + segment_tree[node*2+1]
+
+def query(node, start, end, left, right):
+    propagate(node, start, end)
+    if end < left or start > right:
+        return 0
+    if left <= start and end <= right:
+        return segment_tree[node]
+    mid = (start + end) // 2
+    return query(node*2, start, mid, left, right) + query(node*2+1, mid+1, end, left, right)
+
+build(1, 1, N)
+
 idx = 0
+out = []
 for _ in range(M + K):
     t = queries[idx]
     idx += 1
     if t == 1:
         b, c, d = queries[idx], queries[idx+1], queries[idx+2]
         idx += 3
-        change_tree(1, N, b, c, d, 1)
+        update(1, 1, N, b, c, d)
     else:
         b, c = queries[idx], queries[idx+1]
         idx += 2
-        print(get_sum(1, N, b, c, 1))
+        print(query(1, 1, N, b, c))
