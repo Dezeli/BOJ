46353296e6b00772405ff0260735f4fea787605b
Dezeli <haterecursive@gmail.com>
2025-11-03 10:42:10 +0900

    Solve

---
+++ Python/0~9999/5373.py
@@ -0,0 +1,14 @@
+# 큐빙
+import sys
+
+input = sys.stdin.readline
+
+TC = int(input())
+
+for _ in range(TC):
+    cube = [['w']*9, ['w']*9, ['w']*9, ['w']*9, ['w']*9, ['w']*9]  
+    #       U:윗 면, D:아랫 면, F: 앞면, B: 뒷면, L 왼쪽, R 오른쪽
+
+
+
+    n = int(input())
